784c6d2830c74f3271d6edd6976607018b5ddf78
TamOOP <117593931+TamOOP@users.noreply.github.com>
2022-12-21 20:23:43 +0700

    Add files via upload

---
+++ Main_Tam.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hellofx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main_Tam extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        Scene scene = new Scene(root, 1150, 1000);
+        stage.setTitle("Sổ tay nấu ăn");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
